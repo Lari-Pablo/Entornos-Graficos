6f3aaabf88be67d4dd45c4f07728b577704ba147
Lara Pablo <lara.b.pablo@gmail.com>
2026-05-13 23:21:20 -0300

    Ejercicio 2 de la práctica 5

---
+++ PHP2/Ej2/envio.php
@@ -0,0 +1,18 @@
+<?php
+$destinatario = "webmaster@xx.com";
+$asunto = "Consulta del sitio";
+$fecha = date("d-m-Y");
+$hora = date("H:i:s");
+$desde="From:" .$POST["mail"];
+$consulta="
+\n
+Nombre: $_POST[nombre]\n
+Email: $_POST[mail]\n
+Consulta: $_POST[texto]\n
+Enviado: $fecha a las $hora\n
+\n";
+
+mail($destinatario,$asunto,$consulta,$desde);
+echo "Su consulta ha sido enviada, en breve recibirá nuestra
+respuesta.";
+?>
